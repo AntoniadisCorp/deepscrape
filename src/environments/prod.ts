f38978e5be8ca57a4ef590293cec579e5c5fee3d
prokopis3 <prokopis123@gmail.com>
2025-04-04 04:56:39 +0300

    feat(env): simplify environment variable loading and update dependencies
    - Modified `prod.ts` to directly access parsed `.env` values using `dotenv.config().parsed`. This simplifies the code and improves readability.
    - Removed direct imports of `fs` and `dotenv` in `prod.ts` as they are now accessed via `require`.
    - Removed `@types/node` as a dev dependency and added `node` to the types array in `tsconfig.json`. This ensures that the necessary type definitions for Node.js are available during development.

--- src/environments/prod.ts
+++ src/environments/prod.ts
@@ -1,21 +1,21 @@
 // generate-env.ts
-import * as fs from 'fs';
-import * as dotenv from 'dotenv';
+const fs = require('fs');
+const dotenv = require('dotenv');
 
-dotenv.config();
-export const environment = {
+const env = dotenv.config().parsed; // Get parsed .env values directly
+const environment = {
     production: false,
     assetsUri: "https://deepscrape.web.app/assets/",
     firebaseConfig: {
-        apiKey: process.env["API_KEY"],
-        authDomain: process.env["AUTH_DOMAIN"],
-        databaseURL: process.env["DATABASE_URL"],
-        projectId: process.env["PROJECT_ID"],
-        storageBucket: process.env["STORAGE_BUCKET"],
-        messagingSenderId: process.env["MESSAGING_SENDER_ID"],
-        appId: process.env["APP_ID"],
+        apiKey: env["API_KEY"],
+        authDomain: env["AUTH_DOMAIN"],
+        databaseURL: env["DATABASE_URL"],
+        projectId: env["PROJECT_ID"],
+        storageBucket: env["STORAGE_BUCKET"],
+        messagingSenderId: env["MESSAGING_SENDER_ID"],
+        appId: env["APP_ID"],
     },
-    recpatcha: process.env["RECAPTCHA_KEY"],
+    recpatcha: env["RECAPTCHA_KEY"],
     STRIPE_PUBLIC_KEY: 'pk_test_51OELbJFBBAUAyJFBp3qyNV35TE56uUP9g1IkZ0KOuWnrNLC2ijFoIIkG71xqISwnFwiibjCOumO8itPsLLIyA1py00ABQ2TJ4H',
     OPENAI_API_KEY: '',
     ANTHROPIC_API_KEY: '',
