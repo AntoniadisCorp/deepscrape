59dff37a38225700409ca67e72a2c1361294134f
prokopis3 <prokopis123@gmail.com>
2025-05-07 20:53:10 +0300

    feat(environment): use environment variable for Stripe public key in production
    This commit updates the production environment configuration to use the `STRIPE_PUBLIC_KEY` environment variable instead of a hardcoded value.
    
    - Modified `src/environments/prod.ts` to retrieve the `STRIPE_PUBLIC_KEY` from the environment variables, allowing for easier configuration and updates without requiring code changes.

--- src/environments/prod.ts
+++ src/environments/prod.ts
@@ -15,7 +15,7 @@ const environment = {
         messagingSenderId: env["MESSAGING_SENDER_ID"],
         appId: env["APP_ID"],
     },
-    STRIPE_PUBLIC_KEY: 'pk_test_51OELbJFBBAUAyJFBp3qyNV35TE56uUP9g1IkZ0KOuWnrNLC2ijFoIIkG71xqISwnFwiibjCOumO8itPsLLIyA1py00ABQ2TJ4H',
+    STRIPE_PUBLIC_KEY: env["STRIPE_PUBLIC_KEY"],
     RECAPTCHA_KEY: env["RECAPTCHA_KEY"],
     OPENAI_API_KEY: env["OPENAI_API_KEY"],
     ANTHROPIC_API_KEY: env["ANTHROPIC_API_KEY"],
