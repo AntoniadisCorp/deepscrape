0ad755d9abc666c0414e5f1b0204f6f003e1c63b
prokopis3 <prokopis123@gmail.com>
2025-05-27 14:30:24 +0300

    chore(dependencies): integrate Upstash Redis and Lucide Icons
    This commit introduces Upstash Redis for improved data caching and management, and integrates Lucide Icons for enhanced UI visuals.
    
    - Added `@upstash/redis` and `ioredis` as dependencies for Redis integration.
    - Added `lucide-angular` as a dependency for vector icons.
    - Created `api/config.ts` to initialize and configure the Upstash Redis client using environment variables.
    - Added `src/app/shared/lucideicons.ts` to define and export a set of Lucide icons for use throughout the application.
    - Updated `angular.json` to enable prerendering.

---
+++ api/config.ts
@@ -0,0 +1,60 @@
+// import { CopyCommand, Redis, } from "@upstash/redis"
+import { RedisOptions, Redis } from 'ioredis'
+import chalk from 'chalk'
+// Initialize Upstash Redis client
+// export const redisClient = new Redis({
+//     url: process.env["UPSTASH_REDIS_REST_URL"] || "",
+//     token: process.env["UPSTASH_REDIS_REST_TOKEN"] || "",
+// })
+const host = process.env['UPSTASH_REDIS_REST_URL']
+const port = process.env['UPSTASH_REDIS_REST_PORT']
+const username = process.env['UPSTASH_REDIS_REST_USERNAME'] // Default username for Redis
+const p4ss = process.env['UPSTASH_REDIS_REST_PASSWORD']
+
+const redisOptions: RedisOptions = {
+    host: host || 'localhost', // Read from env
+    port: parseInt(port || '30766'), // Read from env
+    username: username || 'default', // Default username for Redis
+    password: p4ss || '', // Read from env
+    db: 0, // Default database
+    lazyConnect: true, // Use lazy connect to avoid immediate connection
+}
+
+let client: Redis | null = null
+
+// Set username if provided which should resolve the authentication issue with older Redis versions.
+if (username) // Default username for Redis
+    redisOptions.username = username
+
+
+if (host?.length && port?.length && p4ss?.length) {
+    // Configure your Redis client.  IMPORTANT: Use environment variables
+    // for sensitive information like host, port, password.
+    const redisUrl = `rediss://${redisOptions.username}:${redisOptions.password}@${host}:${port}`
+    client = new Redis(redisUrl)
+    console.log(chalk.hex('#028C9E').bold('Upstash Redis client initialized ') + chalk.yellow.bold(`${host}:${port}`))
+    // client.quit() // Ensure we start with a clean slate
+    client.on('reconnecting', () => {
+        console.log('Redis client reconnecting...')
+    })
+    client.on('error', (err) => {
+        console.error('Redis connection error:', err);
+    })
+    client.once('ready', () => {
+        console.log('Redis client ready');
+    })
+    client.on('connect', () => {
+        console.log('Redis client connected')
+    })
+    client.on('end', () => {
+        console.log('Redis client end')
+    })
+    client.on('quit', () => {
+        console.log('Redis client quit')
+    })
+} else {
+    console.error('Redis client not connected: Missing environment variables')
+}
+
+
+export { client as redisClient }
---
+++ src/app/shared/lucideicons.ts
@@ -0,0 +1,37 @@
+import {
+
+    Home,
+    FileText,
+    Zap,
+    Monitor,
+    Search,
+    Globe,
+    Brain,
+    Network,
+    Database,
+    Code,
+    Shield,
+    Gauge,
+    Bot,
+    Menu,
+    LucideIcons,
+    ShieldCheck
+} from 'lucide-angular'
+
+export let myIcons: LucideIcons = {
+    home: Home,
+    menu: Menu,
+    bot: Bot,
+    gauge: Gauge,
+    shield: Shield,
+    database: Database,
+    code: Code,
+    network: Network,
+    brain: Brain,
+    globe: Globe,
+    filetext: FileText,
+    search: Search,
+    monitor: Monitor,
+    zap: Zap,
+    shieldcheck: ShieldCheck
+}
