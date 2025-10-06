42c597cc3e9d54a25403ef3e71ad406ef739dcee
prokopis3 <prokopis123@gmail.com>
2025-10-06 12:08:42 +0300

    fix(config): enable postcss-import plugin and comment out v4 tailwindcss import

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
-
+// import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-    plugins: [],
+    plugins: [
+        // tailwindcss(),
+    ],
     server: {
         port: 4200,  // You can change this to your preferred port
     },
